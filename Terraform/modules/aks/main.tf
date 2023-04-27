fab39bbc8055dc0abb63fa5ea82e8e5d7981f611
owainow <owainosborne@tigerbay.uk.com>
2023-04-27 09:54:22 +0100

    Updated AKS config

--- Terraform/modules/aks/main.tf
+++ Terraform/modules/aks/main.tf
@@ -1,42 +1,68 @@
+data "http" "aksc_release" {
+  url = "https://github.com/Azure/AKS-Construction/releases/download/0.9.13b/main.json"
+  request_headers = {
+    Accept = "application/json"
+    User-Agent = "request module"
+  }
+}
+
 data "azurerm_client_config" "current" {}
 
-locals {
-  arm_template = file("${path.module}/main.json")
+resource "azurerm_resource_group" "rg" {
+  name = var.resourceGroupName
+  location = var.location
 }
 
 resource "azurerm_resource_group_template_deployment" "aksc_deploy" {
-  name                = "AKS-C"
-  resource_group_name = var.resource_group_name
-  deployment_mode     = "Incremental"
-  template_content    = local.arm_template
+  name = "AKS-C"
+  resource_group_name = azurerm_resource_group.rg.name
+  deployment_mode = "Incremental"
+  template_content = data.http.aksc_release.response_body
   parameters_content = jsonencode({
-    automatedDeployment            = { value = var.automated_deployment }
-    resourceName                   = { value = var.cluster_name }
-    managedNodeResourceGroup       = { value = var.node_resource_group_name }
-    aksPaidSkuForSLA               = { value = var.AksPaidSkuForSLA }
-    systemPoolType                 = { value = var.SystemPoolType }
-    agentVMSize                    = { value = var.agentVMSize }
-    nodePoolName                   = { value = var.nodePoolName }
-    agentCountMax                  = { value = var.agentCountMax }
-    osDiskType                     = { value = var.osDiskType }
-    custom_vnet                    = { value = var.custom_vnet }
-    enable_aad                     = { value = var.enable_aad }
-    enableAzureRBAC                = { value = var.enableAzureRBAC }
-    adminPrincipalId               = { value = data.azurerm_client_config.current.object_id }
-    registries_sku                 = { value = var.registries_sku }
-    acrPushRolePrincipalId         = { value = data.azurerm_client_config.current.object_id }
-    privateLinks                   = { value = var.privateLinks }
-    omsagent                       = { value = var.omsagent }
-    retentionInDays                = { value = var.retentionInDays }
-    networkPolicy                  = { value = var.networkPolicy }
-    azurepolicy                    = { value = var.azurepolicy }
-    availabilityZones              = { value = var.availabilityZones }
-    maxPods                        = { value = var.maxPods }
-    enablePrivateCluster           = { value = var.enablePrivateCluster }
-    keyVaultAksCSI                 = { value = var.keyVaultAksCSI }
-    keyVaultCreate                 = { value = var.keyVaultCreate }
-    keyVaultOfficerRolePrincipalId = { value = data.azurerm_client_config.current.object_id }
-    keyVaultAksCSIPollInterval     = { value = var.keyVaultAksCSIPollInterval }
+    resourceName = {value=var.resourceName}
+    managedNodeResourceGroup = {value=var.managedNodeResourceGroup}
+    AksPaidSkuForSLA = {value=var.AksPaidSkuForSLA}
+    SystemPoolType = {value=var.SystemPoolType}
+    agentVMSize = {value=var.agentVMSize}
+    nodePoolName = {value=var.nodePoolName}
+    agentCountMax = {value=var.agentCountMax}
+    osDiskType = {value=var.osDiskType}
+    custom_vnet = {value=var.custom_vnet}
+    CreateNetworkSecurityGroups = {value=var.CreateNetworkSecurityGroups}
+    CreateNetworkSecurityGroupFlowLogs = {value=var.CreateNetworkSecurityGroupFlowLogs}
+    bastion = {value=var.bastion}
+    enable_aad = {value=var.enable_aad}
+    AksDisableLocalAccounts = {value=var.AksDisableLocalAccounts}
+    enableAzureRBAC = {value=var.enableAzureRBAC}
+    adminPrincipalId = {value=data.azurerm_client_config.current.object_id}
+    registries_sku = {value=var.registries_sku}
+    acrPushRolePrincipalId = {value=data.azurerm_client_config.current.object_id}
+    enableACRTrustPolicy = {value=var.enableACRTrustPolicy}
+    azureFirewalls = {value=var.azureFirewalls}
+    azureFirewallsSku = {value=var.azureFirewallsSku}
+    privateLinks = {value=var.privateLinks}
+    keyVaultIPAllowlist = {value=var.keyVaultIPAllowlist}
+    omsagent = {value=var.omsagent}
+    retentionInDays = {value=var.retentionInDays}
+    networkPolicy = {value=var.networkPolicy}
+    azurepolicy = {value=var.azurepolicy}
+    azurePolicyInitiative = {value=var.azurePolicyInitiative}
+    availabilityZones = {value=var.availabilityZones}
+    maxPods = {value=var.maxPods}
+    enablePrivateCluster = {value=var.enablePrivateCluster}
+    aksOutboundTrafficType = {value=var.aksOutboundTrafficType}
+    keyVaultKmsByoKeyId = {value=var.keyVaultKmsByoKeyId}
+    keyVaultKmsByoRG = {value=var.keyVaultKmsByoRG}
+    keyVaultAksCSI = {value=var.keyVaultAksCSI}
+    keyVaultCreate = {value=var.keyVaultCreate}
+    keyVaultOfficerRolePrincipalId = {value=data.azurerm_client_config.current.object_id}
+    fluxGitOpsAddon = {value=var.fluxGitOpsAddon}
+    sgxPlugin = {value=var.sgxPlugin}
+    acrPrivatePool = {value=var.acrPrivatePool}
+    kedaAddon = {value=var.kedaAddon}
+    oidcIssuer = {value=var.oidcIssuer}
+    workloadIdentity = {value=var.workloadIdentity}
+    enableSysLog = {value=var.enableSysLog}
   })
 }
 
--- Terraform/modules/aks/variables.tf
+++ Terraform/modules/aks/variables.tf
@@ -1,106 +1,172 @@
-variable "resource_group_name" {
-  type = string
+#variables.tf
+
+variable resourceGroupName {
+  type=string
+  default="oow-aks-zero-trust-demo"
 }
-variable "node_resource_group_name" {
-  type = string
-}
-variable "location" {
-  type = string
-}
-variable "key_vault_additional_access" {
-  type = list(string)
-}
-variable "cluster_name" {
-  type = string
-}
-variable "automated_deployment" {
-  type    = bool
-  default = true
-}
-variable "upgrade_channel" {
-  type    = string
-  default = "stable"
-}
-variable "AksPaidSkuForSLA" {
-  type    = bool
-  default = true
-}
-variable "SystemPoolType" {
-  type    = string
-  default = "Standard"
-}
-variable "enablePrivateCluster" {
-  type    = bool
-  default = true
-}
-variable "agentVMSize" {
-  type    = string
-  default = "Standard_D4lds_v5"
-}
-variable "nodePoolName" {
-  type    = string
-  default = "userpool01"
-}
-variable "agentCountMax" {
-  type    = number
-  default = 20
-}
-variable "osDiskType" {
-  type    = string
-  default = "Managed"
-}
-variable "custom_vnet" {
-  type    = bool
-  default = true
-}
-variable "enable_aad" {
-  type    = bool
-  default = true
-}
-variable "enableAzureRBAC" {
-  type    = bool
-  default = true
-}
-variable "registries_sku" {
-  type    = string
-  default = "Premium"
-}
-variable "privateLinks" {
-  type    = bool
-  default = true
-}
-variable "omsagent" {
-  type    = bool
-  default = true
-}
-variable "retentionInDays" {
-  type    = number
-  default = 30
-}
-variable "networkPolicy" {
-  type    = string
-  default = "calico"
-}
-variable "azurepolicy" {
-  type    = string
-  default = "audit"
-}
-variable "availabilityZones" {
-  default = ["1", "2", "3"]
-}
-variable "maxPods" {
-  type    = number
-  default = 60
-}
-variable "keyVaultAksCSI" {
-  type    = bool
-  default = true
-}
-variable "keyVaultCreate" {
-  type    = bool
-  default = true
-}
-variable "keyVaultAksCSIPollInterval" {
-  type    = string
-  default = "5m"
+variable location {
+  type=string
+  default="WestEurope"
+} 
+variable resourceName {
+  type=string
+  default="oow-aks-zerotrust"
+} 
+variable managedNodeResourceGroup {
+  type=string
+  default="oow-aks-zerotrust-managed-cluster-resources"
+} 
+variable AksPaidSkuForSLA {
+  type=bool
+  default=true
+} 
+variable SystemPoolType {
+  type=string
+  default="Standard"
+} 
+variable agentVMSize {
+  type=string
+  default="Standard_DC2s_v3"
+} 
+variable nodePoolName {
+  type=string
+  default="sgxNp01"
+} 
+variable agentCountMax {
+  type=number
+  default=20
+} 
+variable osDiskType {
+  type=string
+  default="Managed"
+} 
+variable custom_vnet {
+  type=bool
+  default=true
+} 
+variable CreateNetworkSecurityGroups {
+  type=bool
+  default=true
+} 
+variable CreateNetworkSecurityGroupFlowLogs {
+  type=bool
+  default=true
+} 
+variable bastion {
+  type=bool
+  default=true
+} 
+variable enable_aad {
+  type=bool
+  default=true
+} 
+variable AksDisableLocalAccounts {
+  type=bool
+  default=true
+} 
+variable enableAzureRBAC {
+  type=bool
+  default=true
+} 
+variable registries_sku {
+  type=string
+  default="Premium"
+} 
+variable enableACRTrustPolicy {
+  type=bool
+  default=true
+} 
+variable azureFirewalls {
+  type=bool
+  default=true
+} 
+variable azureFirewallsSku {
+  type=string
+  default="Premium"
+} 
+variable privateLinks {
+  type=bool
+  default=true
+} 
+variable keyVaultIPAllowlist {
+  default=["81.106.57.82/32"]
+} 
+variable omsagent {
+  type=bool
+  default=true
+} 
+variable retentionInDays {
+  type=number
+  default=30
+} 
+variable networkPolicy {
+  type=string
+  default="calico"
+} 
+variable azurepolicy {
+  type=string
+  default="deny"
+} 
+variable azurePolicyInitiative {
+  type=string
+  default="Restricted"
+} 
+variable availabilityZones {
+  default=["1","2","3"]
+} 
+variable maxPods {
+  type=number
+  default=100
+} 
+variable enablePrivateCluster {
+  type=bool
+  default=true
+} 
+variable aksOutboundTrafficType {
+  type=string
+  default="userDefinedRouting"
+} 
+variable keyVaultKmsByoKeyId {
+  type=string
+  default="https://YOURVAULTNAME.vault.azure.net/keys/YOURKEYNAME/KEYVERSIONSTRING"
+} 
+variable keyVaultKmsByoRG {
+  type=string
+  default="oow-aks-zero-trust-demo"
+} 
+variable keyVaultAksCSI {
+  type=bool
+  default=true
+} 
+variable keyVaultCreate {
+  type=bool
+  default=true
+} 
+variable fluxGitOpsAddon {
+  type=bool
+  default=true
+} 
+variable sgxPlugin {
+  type=bool
+  default=true
+} 
+variable acrPrivatePool {
+  type=bool
+  default=true
+} 
+variable kedaAddon {
+  type=bool
+  default=true
+} 
+variable oidcIssuer {
+  type=bool
+  default=true
+} 
+variable workloadIdentity {
+  type=bool
+  default=true
+} 
+variable enableSysLog {
+  type=bool
+  default=true
 }
