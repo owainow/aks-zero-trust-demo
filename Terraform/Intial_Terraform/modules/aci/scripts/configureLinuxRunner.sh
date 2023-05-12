83b5e7d4e536991f0651c5a3e442a588341bb76b
owainow <owainosborne@tigerbay.uk.com>
2023-05-12 16:33:55 +0100

    Added apt-gets

--- Terraform/Intial_Terraform/modules/aci/scripts/configureLinuxRunner.sh
+++ Terraform/Intial_Terraform/modules/aci/scripts/configureLinuxRunner.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Create a folder
 $ mkdir actions-runner && cd actions-runner# Download the latest runner package
 $ curl -o actions-runner-linux-x64-2.304.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.304.0/actions-runner-linux-x64-2.304.0.tar.gz# Optional: Validate the hash
