28c8f3329ea2e7bcea5762fa32e489854bc94762
owainow <owainosborne@tigerbay.uk.com>
2023-05-12 17:01:58 +0100

    runner_as_root

--- Terraform/Intial_Terraform/modules/aci/scripts/configureLinuxRunner.sh
+++ Terraform/Intial_Terraform/modules/aci/scripts/configureLinuxRunner.sh
@@ -4,8 +4,10 @@
 mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.304.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.304.0/actions-runner-linux-x64-2.304.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.304.0.tar.gz
+export RUNNER_ALLOW_RUNASROOT=1
 
 # Create the runner and start the configuration experience
 ./config.sh --url $GH_REPO_URL --token $GH_PAT
+
 # Last step, run it!
 ./run.sh
