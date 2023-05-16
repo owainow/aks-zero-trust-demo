51ed22f73f7669b32434a101857a2eec9eb37911
owainow <owainosborne@tigerbay.uk.com>
2023-05-16 09:44:11 +0100

    Moved outputs.

--- Terraform/Intial_Terraform/modules/aci/main.tf
+++ Terraform/Intial_Terraform/modules/aci/main.tf
@@ -41,11 +41,6 @@ resource "azurerm_container_group" "self_hosted_runners" {
     }
       
 }
-output "GH Repo URL" {
-  value = var.gh_repo_url
-}
-output "GH PAT" {
-  value = var.gh_pat
-}
+
 
 }
---
+++ Terraform/Intial_Terraform/modules/aci/outputs.tf
@@ -0,0 +1,6 @@
+output "GH Repo URL" {
+  value = var.gh_repo_url
+}
+output "GH PAT" {
+  value = var.gh_pat
+}
