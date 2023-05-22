19c5e510b72ac85f4e8dae82917339e35fdb33bf
owainow <owainosborne@tigerbay.uk.com>
2023-05-22 20:21:36 +0100

    Corrected dat source

--- Terraform/Config_Terraform/modules/aks_config/main.tf
+++ Terraform/Config_Terraform/modules/aks_config/main.tf
@@ -4,10 +4,10 @@ data "azurerm_kubernetes_cluster" "aks" {
 }
 
 provider "kubernetes" {
-  host                   = data.azurerm_kubernetes_cluster.main.kube_config.0.host
-  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
-  client_key             = base64decode(data.azurerm_kubernetes_cluster.main.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
+  host                   = data.azurerm_kubernetes_cluster.aks.kube_config.0.host
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
 }
         
 
