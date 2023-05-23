de2960f6b7eca3838861b740e0318d1ea1dedba3
owainow <owainosborne@tigerbay.uk.com>
2023-05-23 10:54:20 +0100

    Removed kubernetes provider

--- Terraform/Config_Terraform/modules/aks_config/main.tf
+++ Terraform/Config_Terraform/modules/aks_config/main.tf
@@ -2,17 +2,6 @@ data "azurerm_kubernetes_cluster" "aks" {
   name                = var.aksclustername
   resource_group_name = var.rg_name
 }
-
-provider "kubernetes" {
-  host                   = data.azurerm_kubernetes_cluster.aks.kube_config.0.host
-  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
-  client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
-}
-
-locals {
-  TF_LOG="DEBUG"
-}
         
 
 
