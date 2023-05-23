aad7eca9aa0ac6d4746e46781dd8febdb87bd920
owainow <owainosborne@tigerbay.uk.com>
2023-05-23 11:07:51 +0100

    Renamed main and moved config to pipeline

--- Terraform/Config_Terraform/modules/aks_config/notusedmain.tf
+++ Terraform/Config_Terraform/modules/aks_config/notusedmain.tf
@@ -15,74 +15,92 @@ resource "kubernetes_manifest" "nginx-pls-namespace" {
 }
 
 resource "kubernetes_manifest" "nginx-pls-sa1" {
+    depends_on = [ kubernetes_manifest.nginx-pls-namespace ]
     manifest = yamldecode(file("${path.module}/nginx_files/pls-nginx-sa1.yaml"))
 }
 
 resource "kubernetes_manifest" "nginx-pls-sa2" {
+    depends_on = [ kubernetes_manifest.nginx-pls-namespace ]
     manifest = yamldecode(file("${path.module}/nginx_files/pls-nginx-sa2.yaml"))
 }
 
 resource "kubernetes_manifest" "nginx-pls-role1" {
+    depends_on = [ kubernetes_manifest.nginx-pls-namespace ]
     manifest = yamldecode(file("${path.module}/nginx_files/pls-nginx-role1.yaml"))
 }
 
 resource "kubernetes_manifest" "nginx-pls-role2" {
+    depends_on = [ kubernetes_manifest.nginx-pls-namespace ]
     manifest = yamldecode(file("${path.module}/nginx_files/pls-nginx-role2.yaml"))
 }
 
 resource "kubernetes_manifest" "nginx-pls-clusterrole1" {
+    depends_on = [ kubernetes_manifest.nginx-pls-namespace ]
     manifest = yamldecode(file("${path.module}/nginx_files/pls-nginx-clusterrole1.yaml"))
 }
 
 resource "kubernetes_manifest" "nginx-pls-clusterrole2" {
+    depends_on = [ kubernetes_manifest.nginx-pls-namespace ]
     manifest = yamldecode(file("${path.module}/nginx_files/pls-nginx-clusterrole2.yaml"))
 }
 
 resource "kubernetes_manifest" "nginx-pls-rolebinding1" {
+    depends_on = [ kubernetes_manifest.nginx-pls-namespace ]
     manifest = yamldecode(file("${path.module}/nginx_files/pls-nginx-rolebinding1.yaml"))
 }
 
 resource "kubernetes_manifest" "nginx-pls-rolebinding2" {
+    depends_on = [ kubernetes_manifest.nginx-pls-namespace ]
     manifest = yamldecode(file("${path.module}/nginx_files/pls-nginx-rolebinding2.yaml"))
 }
 
 resource "kubernetes_manifest" "nginx-pls-clusterrolebinding1" {
+    depends_on = [ kubernetes_manifest.nginx-pls-namespace ]
     manifest = yamldecode(file("${path.module}/nginx_files/pls-nginx-clusterrb1.yaml"))
 }
 
 resource "kubernetes_manifest" "nginx-pls-clusterrolebinding2" {
+    depends_on = [ kubernetes_manifest.nginx-pls-namespace ]
     manifest = yamldecode(file("${path.module}/nginx_files/pls-nginx-clusterrb2.yaml"))
 }
 
 resource "kubernetes_manifest" "nginx-pls-configmap" {
+    depends_on = [ kubernetes_manifest.nginx-pls-namespace ]
     manifest = yamldecode(file("${path.module}/nginx_files/pls-nginx-configmap.yaml"))
 }
 
 resource "kubernetes_manifest" "nginx-pls-service1" {
+    depends_on = [ kubernetes_manifest.nginx-pls-namespace ]
     manifest = yamldecode(file("${path.module}/nginx_files/pls-nginx-service1.yaml"))
 }
 
 resource "kubernetes_manifest" "nginx-pls-service2" {
+    depends_on = [ kubernetes_manifest.nginx-pls-namespace ]
     manifest = yamldecode(file("${path.module}/nginx_files/pls-nginx-service2.yaml"))
 }
 
 resource "kubernetes_manifest" "nginx-pls-deployment" {
+    depends_on = [ kubernetes_manifest.nginx-pls-namespace ]
     manifest = yamldecode(file("${path.module}/nginx_files/pls-nginx-deployment.yaml"))
 }
 
 resource "kubernetes_manifest" "nginx-pls-job1" {
+    depends_on = [ kubernetes_manifest.nginx-pls-namespace ]
     manifest = yamldecode(file("${path.module}/nginx_files/pls-nginx-job1.yaml"))
 }
 
 resource "kubernetes_manifest" "nginx-pls-job2" {
+    depends_on = [ kubernetes_manifest.nginx-pls-namespace ]
     manifest = yamldecode(file("${path.module}/nginx_files/pls-nginx-job2.yaml"))
 }
 
 resource "kubernetes_manifest" "nginx-pls-ingressclass" {
+    depends_on = [ kubernetes_manifest.nginx-pls-namespace ]
     manifest = yamldecode(file("${path.module}/nginx_files/pls-nginx-ingressclass.yaml"))
 }
 
 resource "kubernetes_manifest" "nginx-pls-webhookval" {
+    depends_on = [ kubernetes_manifest.nginx-pls-namespace ]
     manifest = yamldecode(file("${path.module}/nginx_files/pls-nginx-webhookval.yaml"))
 }
 
