5ed67277ea09cd1dfeba0e420fbe7f80e6d3d03d
owainow <owainosborne@tigerbay.uk.com>
2023-06-05 16:59:39 +0100

    Added rule collection for image repos

--- Terraform/Intial_Terraform/modules/aks/main.tf
+++ Terraform/Intial_Terraform/modules/aks/main.tf
@@ -71,69 +71,25 @@ data "azurerm_firewall" "aks-egress-firewall" {
   resource_group_name = var.resourceGroupName
 }
 
-# Wait for 5 minutes before creating node pool to ensure that the cluster is ready
-#resource "time_sleep" "wait_5_Minutes" {
-#  depends_on = [azurerm_resource_group_template_deployment.aksc_deploy]
-#
-#  create_duration = "5m"
-#}
-
-#resource "azurerm_kubernetes_cluster_node_pool" "np1" {
-# depends_on = [ time_sleep.wait_5_Minutes ]
-#  name                  = "fipsnp01"
-#  kubernetes_cluster_id = local.arm_outputs.aksResourceId.value
-#  vm_size               = var.agentVMSize
-#  node_count            = 3
-#  fips_enabled = true
-#  zones = [1, 2, 3]
-#
-#  tags = {
-#    Environment = "fips_enabled"
-#  }
-#}
-
-resource "azurerm_kubernetes_cluster_node_pool" "np2" {
-  depends_on = [ azurerm_resource_group_template_deployment.aksc_deploy ]
-  name                  = "cfdnp01"
-  kubernetes_cluster_id = local.arm_outputs.aksResourceId.value
-  vm_size               = "Standard_DC2s_v3"
-  node_count            = 1
-  
-
-
-  tags = {
-    Environment = "confidental_compute"
-  }
-}
-
-data "azurerm_user_assigned_identity" "aks_uai" {
-  depends_on = [ azurerm_kubernetes_cluster_node_pool.np2 ]
-  name                = "id-aks-${var.resourceName}"
+data "azurerm_firewall_policy" "aks-egress-policy" {
+  name                = "afwp-${var.resourceName}"
   resource_group_name = var.resourceGroupName
 }
 
-resource "azurerm_key_vault_access_policy" "etcd_uai" {
-  depends_on = [ azurerm_kubernetes_cluster_node_pool.np2 ]
-  key_vault_id = data.azurerm_key_vault.aks.id
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_user_assigned_identity.aks_uai.principal_id
-  key_permissions    = [ "UnwrapKey", "WrapKey", "Encrypt", "Decrypt", "Sign", "Verify"]
-
-}
-
-resource "azurerm_firewall_network_rule_collection" "image_repo" {
-  depends_on = [ azurerm_key_vault_access_policy.etcd_uai ]
-  name                = "image_repo_ip_allowlist"
-  azure_firewall_name = data.azurerm_firewall.aks-egress-firewall.name
-  resource_group_name = var.resourceGroupName
-  priority            = 100
-  action              = "Allow"
-
-  rule {
-    name = "quay"
+data "azurerm_firewall_policy_rule_collection_group" "aks-egress-firewall" {
+  depends_on = [ data.azurerm_firewall.aks-egress-firewall ]
+  name                = "afw-${var.resourceName}-tf-rule-collection-group"
+  firewall_policy_id = data.azurerm_firewall_policy.aks-egress-policy.id
+  priority = 100
+  network_rule_collection {
+    name = "image-repo_collection"
+    action = "Allow"
+    priority = 100
+    rule {
+    name = "quay_dockerhub_ghcr_IpAllowList"
 
     source_addresses = [
-      "10.240.0.0/22",
+      "10.240.0.0/22"
     ]
 
     destination_ports = [
@@ -187,4 +143,56 @@ resource "azurerm_firewall_network_rule_collection" "image_repo" {
       "TCP"
     ]
   }
+  }
 }
+
+# Wait for 5 minutes before creating node pool to ensure that the cluster is ready
+#resource "time_sleep" "wait_5_Minutes" {
+#  depends_on = [azurerm_resource_group_template_deployment.aksc_deploy]
+#
+#  create_duration = "5m"
+#}
+
+#resource "azurerm_kubernetes_cluster_node_pool" "np1" {
+# depends_on = [ time_sleep.wait_5_Minutes ]
+#  name                  = "fipsnp01"
+#  kubernetes_cluster_id = local.arm_outputs.aksResourceId.value
+#  vm_size               = var.agentVMSize
+#  node_count            = 3
+#  fips_enabled = true
+#  zones = [1, 2, 3]
+#
+#  tags = {
+#    Environment = "fips_enabled"
+#  }
+#}
+
+resource "azurerm_kubernetes_cluster_node_pool" "np2" {
+  depends_on = [ azurerm_resource_group_template_deployment.aksc_deploy ]
+  name                  = "cfdnp01"
+  kubernetes_cluster_id = local.arm_outputs.aksResourceId.value
+  vm_size               = "Standard_DC2s_v3"
+  node_count            = 1
+  
+
+
+  tags = {
+    Environment = "confidental_compute"
+  }
+}
+
+data "azurerm_user_assigned_identity" "aks_uai" {
+  depends_on = [ azurerm_kubernetes_cluster_node_pool.np2 ]
+  name                = "id-aks-${var.resourceName}"
+  resource_group_name = var.resourceGroupName
+}
+
+resource "azurerm_key_vault_access_policy" "etcd_uai" {
+  depends_on = [ azurerm_kubernetes_cluster_node_pool.np2 ]
+  key_vault_id = data.azurerm_key_vault.aks.id
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_user_assigned_identity.aks_uai.principal_id
+  key_permissions    = [ "UnwrapKey", "WrapKey", "Encrypt", "Decrypt", "Sign", "Verify"]
+
+}
+
