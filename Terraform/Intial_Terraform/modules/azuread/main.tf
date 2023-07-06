d769f5005b6abb16bcd6ebe534cc7e753d6c0b99
owainow <owainosborne@tigerbay.uk.com>
2023-07-06 17:46:14 +0100

    Minor typo

--- Terraform/Intial_Terraform/modules/azuread/main.tf
+++ Terraform/Intial_Terraform/modules/azuread/main.tf
@@ -11,6 +11,6 @@ resource "azuread_user" "demo_user" {
 resource "azuread_group" "demo_aks_jit_group" {
   depends_on = [ azuread_user.demo_user ]
   display_name     = "oow-aks-jit-azure-cvm-attestation-ns"
-  owners           = [data.azuread_client_config.current.object_id]
+  owners           = [data.azurerm_client_config.current.object_id]
   security_enabled = true
 }
